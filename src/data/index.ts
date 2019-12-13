bd73a1c67e7be41c60f663ac3d0ccf659d525c74
Joakim <jole18ag@student.cbs.dk>
2019-12-13 23:07:00 +0100

    import all files

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,23 @@
-import may2019 from './may2019'
-import dec2018 from './dec2018'
+import aug2016 from "./aug2016";
+import aug2017 from "./aug2017";
+import dec2016 from "./dec2016";
+import dec2017 from "./dec2017";
+import dec2018 from "./dec2018";
+import may2016 from "./may2016";
+import may2017 from "./may2017";
+import may2018 from "./may2018";
+import may2019 from "./may2019";
 
-const exportable: {[id: string]: string[]} = {may2019, dec2018}
+const exportable: { [id: string]: string[] } = {
+  aug2016,
+  aug2017,
+  dec2016,
+  dec2017,
+  dec2018,
+  may2016,
+  may2017,
+  may2018,
+  may2019
+};
 
-export default exportable
+export default exportable;
